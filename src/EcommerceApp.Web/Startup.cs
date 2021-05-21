f7b02df5568f465204c117a2708e807ac51a5145
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-21 22:32:53 +0200

    Scaffolded Register and Login from Identity

--- src/EcommerceApp.Web/Startup.cs
+++ src/EcommerceApp.Web/Startup.cs
@@ -57,6 +57,7 @@ namespace EcommerceApp.Web
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
