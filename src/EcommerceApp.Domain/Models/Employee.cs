13db9dfdc602c83f0bd98168c1d7c8a3e3003cf5
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-26 21:45:24 +0200

    Rebuild Employee model, made foreignkey AppUserId

--- src/EcommerceApp.Domain/Models/Employee.cs
+++ src/EcommerceApp.Domain/Models/Employee.cs
@@ -6,7 +6,7 @@ namespace EcommerceApp.Domain.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
-        public string EmailAddress { get; set; }
-        public string Password { get; set; }
+        public string AppUserId { get; set; }
+        public ApplicationUser AppUser { get; set; }
     }
 }
