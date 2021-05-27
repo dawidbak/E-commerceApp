dd44261576f4eafa3577b7f4b0c50647045a4614
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-27 15:49:12 +0200

    Created EmployeeVM entity

---
+++ src/EcommerceApp.Application/ViewModels/EmployeeVM.cs
@@ -0,0 +1,16 @@
+using System;
+using AutoMapper;
+using EcommerceApp.Application.Mapping;
+
+
+namespace EcommerceApp.Application.ViewModels
+{
+    public class EmployeeVM : IMapFrom<Domain.Models.Employee>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+
+        public void Mapping(Profile profile) => profile.CreateMap<Domain.Models.Employee, EmployeeVM>();
+    }
+}
