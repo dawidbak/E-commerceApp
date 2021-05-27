30548ae07953158779dabd5490e95c6a41de3aed
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-27 16:28:12 +0200

    Create EmployeeServiceUnitTests.cs

---
+++ tests/EcommerceApp.Application.Tests/Services/UnitTests/EmployeeServiceUnitTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EcommerceApp.Application.Tests.Services.UnitTests
+{
+    public class EmployeeServiceUnitTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            
+        }
+    }
+}
