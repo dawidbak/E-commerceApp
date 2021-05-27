ec06a2c944041e8434003536061f2c21ada9ace9
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-27 22:38:45 +0200

    Correct the suffixes and change return type of GetAll to List from IQueryable

--- src/EcommerceApp.Application/Services/EmployeeService.cs
+++ src/EcommerceApp.Application/Services/EmployeeService.cs
@@ -6,6 +6,8 @@ using EcommerceApp.Application.Interfaces;
 using EcommerceApp.Application.ViewModels;
 using EcommerceApp.Domain.Interfaces;
 using EcommerceApp.Domain.Models;
+using AutoMapper.QueryableExtensions;
+using System.Collections.Generic;
 
 namespace EcommerceApp.Application.Services
 {
@@ -18,25 +20,26 @@ namespace EcommerceApp.Application.Services
             _mapper = mapper;
             _employeeRepository = employeeRepository;
         }
-        public async Task AddEmployeeAsync(EmployeeVM employee)
+        public async Task AddEmployeeAsync(EmployeeVM employeeVM)
         {
-            var employeeVM = _mapper.Map<Employee>(employee);
-            await _employeeRepository.AddEmployeeAsync(employeeVM);
+            var employee = _mapper.Map<Employee>(employeeVM);
+            await _employeeRepository.AddEmployeeAsync(employee);
         }
-        public async Task<IQueryable<EmployeeVM>> GetAllEmployeesAsync()
+        public async Task<List<EmployeeVM>> GetAllEmployeesAsync()
         {
-            var employees = await _employeeRepository.GetAllEmployeesAsync();
-            return _mapper.Map<IQueryable<EmployeeVM>>(employees);
+            var employees = (await _employeeRepository.GetAllEmployeesAsync()).ToList();
+            return _mapper.Map<List<EmployeeVM>>(employees);
+            
         }
         public async Task<EmployeeVM> GetEmployeeAsync(int id)
         {
             var employee = await _employeeRepository.GetEmployeeAsync(id);
             return _mapper.Map<EmployeeVM>(employee);
         }
-        public async Task UpdateEmployeeAsync(EmployeeVM employee)
+        public async Task UpdateEmployeeAsync(EmployeeVM employeeVM)
         {
-            var employeeVM = _mapper.Map<Employee>(employee);
-            await _employeeRepository.UpdateEmployeeAsync(employeeVM);
+            var employee = _mapper.Map<Employee>(employeeVM);
+            await _employeeRepository.UpdateEmployeeAsync(employee);
         }
         public async Task DeleteEmployeeAsync(int id)
         {
