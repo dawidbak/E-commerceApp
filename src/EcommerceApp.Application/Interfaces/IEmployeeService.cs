d2ce9e7a9ae7df4d678d1062850c7d4b427b2a73
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-27 15:49:42 +0200

    Create IEmployeeService and make all CRUD methods

--- src/EcommerceApp.Application/Interfaces/IEmployeeService.cs
+++ src/EcommerceApp.Application/Interfaces/IEmployeeService.cs
@@ -1,7 +1,15 @@
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceApp.Application.ViewModels;
+
 namespace  EcommerceApp.Application.Interfaces
 {
     public interface IEmployeeService
     {
-        
+        Task AddEmployeeAsync(EmployeeVM employee);
+        Task<EmployeeVM> GetEmployeeAsync(int id);
+        Task<IQueryable<EmployeeVM>> GetAllEmployeesAsync();
+        Task UpdateEmployeeAsync(EmployeeVM employee);
+        Task DeleteEmployeeAsync(int id);
     }
 }
