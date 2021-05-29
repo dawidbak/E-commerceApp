834517fcf06c50275ffbb21a3abe52542f502a00
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-29 20:01:46 +0200

    Add Email property to entity for show additional data in Views

--- src/EcommerceApp.Domain/Models/Employee.cs
+++ src/EcommerceApp.Domain/Models/Employee.cs
@@ -6,6 +6,7 @@ namespace EcommerceApp.Domain.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
+        public string Email { get; set; }
         public string AppUserId { get; set; }
         public ApplicationUser AppUser { get; set; }
     }
