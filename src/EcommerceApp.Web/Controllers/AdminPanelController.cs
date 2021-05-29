90937b7ca27b8ed88feb87d1c13a13b34a60c6e4
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-29 02:00:47 +0200

    Created AdminPanelController with all required methods

--- src/EcommerceApp.Web/Controllers/AdminPanelController.cs
+++ src/EcommerceApp.Web/Controllers/AdminPanelController.cs
@@ -8,16 +8,55 @@ using Microsoft.Extensions.Logging;
 using EcommerceApp.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using EcommerceApp.Infrastructure.Repositories;
+using EcommerceApp.Application.Interfaces;
+using EcommerceApp.Application.ViewModels;
 
 namespace EcommerceApp.Web.Controllers
 {
+    [Authorize("Admin")]
     public class AdminPanelController : Controller
     {
-        
-
-        public AdminPanelController(EmployeeRepository employeeRepository)
+        private readonly IEmployeeService _employeeService;
+        public AdminPanelController(IEmployeeService employeeService)
         {
-            
+            _employeeService = employeeService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _employeeService.GetAllEmployeesAsync();
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeVM employeeVM)
+        {
+            await _employeeService.AddEmployeeAsync(employeeVM);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteEmployee(int Id)
+        {
+            await _employeeService.DeleteEmployeeAsync(Id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditEmployee(int id)
+        {
+            var model = await _employeeService.GetEmployeeAsync(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEmployee(EmployeeVM employeeVM)
+        {
+            await _employeeService.UpdateEmployeeAsync(employeeVM);
+            return RedirectToAction("Index");
         }
         
     }
