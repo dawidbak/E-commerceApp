eb29fe3e6e84a6071d7f3aacf955a3f30a70fd97
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-29 19:59:41 +0200

    Added two property(Email,Password) to add employees via identity

--- src/EcommerceApp.Application/ViewModels/EmployeeVM.cs
+++ src/EcommerceApp.Application/ViewModels/EmployeeVM.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using AutoMapper;
 using EcommerceApp.Application.Mapping;
 
@@ -8,6 +9,16 @@ namespace EcommerceApp.Application.ViewModels
     public class EmployeeVM : IMapFrom<Domain.Models.Employee>
     {
         public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
