9d7c43a48bd1bcc22035b23774119bea38bdc27c
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-05-29 19:56:53 +0200

    Added new column "Email"

---
+++ src/EcommerceApp.Infrastructure/Migrations/20210529174218_AddEmailToEmployee.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceApp.Infrastructure.Migrations
+{
+    public partial class AddEmailToEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+        }
+    }
+}
