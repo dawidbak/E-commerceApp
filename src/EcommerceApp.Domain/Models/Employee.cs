d4bf6be181662aca51d0d978984b5f07214cb7a4
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-02 17:13:16 +0200

    Added DataAnnotations

--- src/EcommerceApp.Domain/Models/Employee.cs
+++ src/EcommerceApp.Domain/Models/Employee.cs
@@ -1,13 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EcommerceApp.Domain.Models
 {
     public class Employee
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [MinLength(2)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Position { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string AppUserId { get; set; }
+
+        [ForeignKey("AppUserId")]
         public ApplicationUser AppUser { get; set; }
     }
 }
