af18f20373914d9660dd736a9f62d813a2b9a51a
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-02 18:23:30 +0200

    Added interface for Category Repository and created all methods in CategoryRepository

---
+++ src/EcommerceApp.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceApp.Domain.Models;
+
+namespace EcommerceApp.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task AddCategoryAsync(Category category);
+        Task<Category> GetCategoryAsync(int categoryId);
+        Task<IQueryable<Category>> GetAllCategoriesAsync();
+        Task UpdateCategoryAsync(Category Category);
+        Task DeleteCategoryAsync(int categoryId);
+    }
+}
---
+++ src/EcommerceApp.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceApp.Domain.Interfaces;
+using EcommerceApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApp.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddCategoryAsync(Category category)
+        {
+            await _appDbContext.Categories.AddAsync(category);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategoryAsync(int categoryId)
+        {
+            var category = await _appDbContext.Categories.FindAsync(categoryId);
+            if(category != null)
+            {
+                _appDbContext.Categories.Remove(category);
+                await _appDbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IQueryable<Category>> GetAllCategoriesAsync()
+        {
+            return (await _appDbContext.Categories.ToListAsync()).AsQueryable();
+        }
+
+        public async Task<Category> GetCategoryAsync(int categoryId)
+        {
+            return await _appDbContext.Categories.FindAsync(categoryId);
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _appDbContext.Categories.Update(category);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
