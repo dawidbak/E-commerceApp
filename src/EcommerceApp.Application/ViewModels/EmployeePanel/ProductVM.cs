ee6db5e269a6caf496c828683f923999ea151d22
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-03 18:35:45 +0200

    Created Product View Model with validations rules

---
+++ src/EcommerceApp.Application/ViewModels/EmployeePanel/ProductVM.cs
@@ -0,0 +1,32 @@
+using System.Data;
+using System;
+using EcommerceApp.Application.Mapping;
+using AutoMapper;
+using FluentValidation;
+
+namespace EcommerceApp.Application.ViewModels.EmployeePanel
+{
+    public class ProductVM : IMapFrom<Domain.Models.Product>
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public byte[] Picture { get; set; }
+
+        public void Mapping(Profile profile) => profile.CreateMap<Domain.Models.Product, ProductVM>().ReverseMap();
+    }
+
+    public class ProductValidator : AbstractValidator<Domain.Models.Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductId).NotNull();
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Description).MaximumLength(200);
+            RuleFor(x => x.UnitPrice).NotEmpty().ScalePrecision(2,18);
+            RuleFor(x => x.UnitsInStock).NotNull();
+        }
+    }
+}
