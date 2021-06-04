1de0f8b45bcec6b8a5959a3381d22f244359060f
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-04 20:56:21 +0200

    Register policy for authorize in EmployePanel controller

--- src/EcommerceApp.Application/DependencyInjection.cs
+++ src/EcommerceApp.Application/DependencyInjection.cs
@@ -6,6 +6,8 @@ using EcommerceApp.Application.ViewModels.AdminPanel;
 using FluentValidation.AspNetCore;
 using FluentValidation;
 using EcommerceApp.Application.ViewModels.EmployeePanel;
+using EcommerceApp.Application.Policies.CanAccessEmployeePanel;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EcommerceApp.Application
 {
@@ -14,14 +16,20 @@ namespace EcommerceApp.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            services.AddScoped<IEmployeeService,EmployeeService>();
-            services.AddScoped<ICategoryService,CategoryService>();
-            services.AddScoped<IProductService,ProductService>();
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
             services.AddMvc().AddFluentValidation();
             services.AddTransient<IValidator<EmployeeVM>, EmployeeValidator>();
             services.AddTransient<IValidator<CategoryVM>, CategoryValidator>();
             services.AddTransient<IValidator<ProductVM>, ProductValidator>();
-            
+            services.AddSingleton<IAuthorizationHandler, HasAdminClaimHandler>();
+            services.AddSingleton<IAuthorizationHandler, HasIsEmployeeClaimHandler>();
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("EmployeePanelEntry", policyBuilder => policyBuilder.Requirements.Add(new CanAccessEmployeePanelRequirement()));
+            });
+
             return services;
         }
     }
