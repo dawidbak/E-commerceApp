f3f4152d5b8f317201d9a5dd5de8b40ba06bc382
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-05 21:27:29 +0200

    Migrated new name of property

---
+++ src/EcommerceApp.Infrastructure/Migrations/20210605151603_Image.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceApp.Infrastructure.Migrations
+{
+    public partial class Image : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Picture",
+                table: "Products",
+                newName: "Image");
+
+            migrationBuilder.RenameColumn(
+                name: "Picture",
+                table: "Categories",
+                newName: "Image");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Products",
+                newName: "Picture");
+
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Categories",
+                newName: "Picture");
+        }
+    }
+}
