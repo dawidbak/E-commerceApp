b405199a2155b786025c58bd05dedf8cc323caaa
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-09 19:26:45 +0200

    Create CategoryController.cs

---
+++ src/EcommerceApp.Web/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApp.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IProductService _productService;
+        public CategoryController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> Products(string categoryName)
+        {
+            var model = await _productService.GetProductsByCategoryNameAsync(categoryName);
+            return View(model);
+        }
+    }
+}
