4c889b28db34eed89ed0d5fb5e495f0ec76c3c41
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-10 19:11:37 +0200

    Extended registration form

--- src/EcommerceApp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/EcommerceApp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,21 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using EcommerceApp.Domain.Interfaces;
+using EcommerceApp.Domain.Models;
+using EcommerceApp.Infrastructure;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using EcommerceApp.Infrastructure;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
-using System.Security.Claims;
-using EcommerceApp.Domain.Models;
-
 
 namespace EcommerceApp.Web.Areas.Identity.Pages.Account
 {
@@ -27,17 +27,20 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly ICustomerRepository _customerRepository;
 
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            ICustomerRepository customerRepository)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _customerRepository = customerRepository;
         }
 
         [BindProperty]
@@ -65,8 +68,38 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
-            [Display(Name ="isAdmin")]
-            public bool IsAdmin{get;set;}
+            [Required]
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="City")]
+            public string City { get; set; }
+
+            [Required]
+            [StringLength(10, MinimumLength = 5,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="Postal Code")]
+            public string PostalCode { get; set; }
+
+            [Required]
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="Address")]
+            public string Address { get; set; }
+
+            [Required]
+            [Phone]
+            [Display(Name ="Phone")]
+            public string PhoneNumber { get; set; }
+
+            [Display(Name = "isAdmin")]
+            public bool IsAdmin { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -81,10 +114,14 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, PhoneNumber = Input.PhoneNumber };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    var customer = new Customer{FirstName = Input.FirstName, LastName = Input.LastName, City = Input.City, PostalCode = Input.PostalCode,
+                    Address = Input.Address, AppUserId = user.Id};
+
+                    await _customerRepository.AddCustomerAsync(customer);
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
@@ -98,8 +135,8 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    var claim = new Claim("Admin",Input.IsAdmin.ToString());
-                    await _userManager.AddClaimAsync(user,claim);
+                    var claim = new Claim("Admin", Input.IsAdmin.ToString());
+                    await _userManager.AddClaimAsync(user, claim);
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
