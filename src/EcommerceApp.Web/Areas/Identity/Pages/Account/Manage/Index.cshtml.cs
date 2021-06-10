61b9f314f6340dd667a1805213878a89cd2bef75
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-10 20:33:36 +0200

    Extended account update form

--- src/EcommerceApp.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/EcommerceApp.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using EcommerceApp.Domain.Interfaces;
 using EcommerceApp.Domain.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,13 +15,16 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ICustomerRepository _customerRepository;
 
         public IndexModel(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+            ICustomerRepository customerRepository)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _customerRepository = customerRepository;
         }
 
         public string Username { get; set; }
@@ -33,6 +37,26 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="First Name")]
+            public string FirstName { get; set; }
+
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="Last Name")]
+            public string LastName { get; set; }
+
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="City")]
+            public string City { get; set; }
+
+            [StringLength(10, MinimumLength = 5,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="Postal Code")]
+            public string PostalCode { get; set; }
+
+            [StringLength(50, MinimumLength = 2,ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+            [Display(Name ="Address")]
+            public string Address { get; set; }
+
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
@@ -42,11 +66,18 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account.Manage
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var customerId = await _customerRepository.GetCustomerIdAsync(user.Id);
+            var customer = await _customerRepository.GetCustomerAsync(customerId);
 
             Username = userName;
 
             Input = new InputModel
             {
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                City = customer.City,
+                PostalCode = customer.PostalCode,
+                Address = customer.Address,
                 PhoneNumber = phoneNumber
             };
         }
@@ -66,6 +97,7 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account.Manage
         public async Task<IActionResult> OnPostAsync()
         {
             var user = await _userManager.GetUserAsync(User);
+            
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
@@ -76,6 +108,19 @@ namespace EcommerceApp.Web.Areas.Identity.Pages.Account.Manage
                 await LoadAsync(user);
                 return Page();
             }
+
+            var customer = new Customer
+            {
+                Id = await _customerRepository.GetCustomerIdAsync(user.Id),
+                FirstName = Input.FirstName,
+                LastName = Input.LastName,
+                City = Input.City,
+                PostalCode = Input.PostalCode,
+                Address = Input.Address,
+                AppUserId = user.Id,
+            };
+
+            await _customerRepository.UpdateCustomerAsync(customer);
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             if (Input.PhoneNumber != phoneNumber)
