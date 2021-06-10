145fd463f1cbb050694862bc536662ecd67c9f6b
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-10 19:14:56 +0200

    Created repository for Customers table

---
+++ src/EcommerceApp.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceApp.Domain.Models;
+
+namespace EcommerceApp.Domain.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task AddCustomerAsync(Customer customer);
+        Task<IQueryable<Customer>> GetAllCustomersAsync();
+        Task<Customer> GetCustomerAsync(int customerId);
+        Task UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(int customerId);
+    }
+}
---
+++ src/EcommerceApp.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceApp.Domain.Interfaces;
+using EcommerceApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApp.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CustomerRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            await _appDbContext.AddAsync(customer);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IQueryable<Customer>> GetAllCustomersAsync()
+        {
+            return (await _appDbContext.Customers.ToListAsync()).AsQueryable();
+        }
+
+        public async Task<Customer> GetCustomerAsync(int customerId)
+        {
+            return await _appDbContext.Customers.FindAsync(customerId);
+        }
+
+        public async Task UpdateCustomerAsync(Customer customer)
+        {
+            _appDbContext.Update(customer);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteCustomerAsync(int customerId)
+        {
+            var customer = await _appDbContext.Customers.FindAsync(customerId);
+            if (customer != null)
+            {
+                _appDbContext.Remove(customer);
+                await _appDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
