574aea8d66fe3df7382a5d4b28ca8420600dd2a1
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-11 19:56:01 +0200

    Formatted and added two separete method to Employees and Customers and added DeleteCustomer method

--- src/EcommerceApp.Web/Controllers/AdminPanelController.cs
+++ src/EcommerceApp.Web/Controllers/AdminPanelController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using EcommerceApp.Web.Models;
-using Microsoft.AspNetCore.Authorization;
-using EcommerceApp.Infrastructure.Repositories;
 using EcommerceApp.Application.Interfaces;
 using EcommerceApp.Application.ViewModels.AdminPanel;
+using EcommerceApp.Infrastructure.Repositories;
+using EcommerceApp.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EcommerceApp.Web.Controllers
 {
@@ -18,15 +18,22 @@ namespace EcommerceApp.Web.Controllers
     {
         private readonly IEmployeeService _employeeService;
         private readonly ISearchService _searchService;
+        private readonly ICustomerService _customerService;
         private readonly ILogger<AdminPanelController> _logger;
-        public AdminPanelController(IEmployeeService employeeService,ILogger<AdminPanelController> logger, ISearchService searchService)
+        public AdminPanelController(IEmployeeService employeeService, ILogger<AdminPanelController> logger, ISearchService searchService, ICustomerService customerService)
         {
             _employeeService = employeeService;
             _logger = logger;
             _searchService = searchService;
+            _customerService = customerService;
         }
 
-        public async Task<IActionResult> Index(string selectedValue, string searchString)
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Employees(string selectedValue, string searchString)
         {
             if (!string.IsNullOrEmpty(selectedValue) && !string.IsNullOrEmpty(searchString))
             {
@@ -34,6 +41,16 @@ namespace EcommerceApp.Web.Controllers
             }
             return View(await _employeeService.GetAllEmployeesAsync());
         }
+
+        public async Task<IActionResult> Customers(string selectedValue, string searchString)
+        {
+            if (!string.IsNullOrEmpty(selectedValue) && !string.IsNullOrEmpty(searchString))
+            {
+                return View(await _searchService.SearchSelectedCustomerAsync(selectedValue, searchString));
+            }
+            return View(await _customerService.GetAllCustomersAsync());
+        }
+
         [HttpGet]
         public IActionResult AddEmployee()
         {
@@ -60,6 +77,17 @@ namespace EcommerceApp.Web.Controllers
             await _employeeService.DeleteEmployeeAsync(id.Value);
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> DeleteCustomer(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound("You must pass a valid Customer ID in the route, for example, /AdminPanel/DeleteCustomer/21");
+            }
+            await _customerService.DeleteCustomerAsync(id.Value);
+            return RedirectToAction("Index");
+        }
+
         [HttpGet]
         public async Task<IActionResult> EditEmployee(int? id)
         {
