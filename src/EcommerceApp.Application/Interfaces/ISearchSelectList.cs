45dcfbcdbb6ef8bb705561ff06eef648419fd622
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-11 19:54:07 +0200

    Added method for Customer search form

--- src/EcommerceApp.Application/Interfaces/ISearchSelectList.cs
+++ src/EcommerceApp.Application/Interfaces/ISearchSelectList.cs
@@ -9,5 +9,6 @@ namespace EcommerceApp.Application.Interfaces
         List<SelectListItem> CategorySelectList{get;}
         List<SelectListItem> ProductSelectList{get;}
         List<SelectListItem> EmployeeSelectList{get;}
+        List<SelectListItem> CustomerSelectList{get;}
     }
 }
