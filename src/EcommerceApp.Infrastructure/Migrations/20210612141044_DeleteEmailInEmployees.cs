460850f1ac0c5f0310a04d164782bece16887b6e
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-12 18:02:12 +0200

    Migrated changes in Employee table

---
+++ src/EcommerceApp.Infrastructure/Migrations/20210612141044_DeleteEmailInEmployees.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceApp.Infrastructure.Migrations
+{
+    public partial class DeleteEmailInEmployees : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
