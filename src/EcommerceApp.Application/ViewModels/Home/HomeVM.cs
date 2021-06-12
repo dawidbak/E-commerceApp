f58fd29c7519a88a4dc3eaf9639e2a5aa3405861
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-12 19:24:21 +0200

    Deleted property of Categories, it's no more needed

--- src/EcommerceApp.Application/ViewModels/Home/HomeVM.cs
+++ src/EcommerceApp.Application/ViewModels/Home/HomeVM.cs
@@ -1,13 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using EcommerceApp.Application.ViewModels.EmployeePanel;
+using EcommerceApp.Application.ViewModels.Product;
 
 namespace EcommerceApp.Application.ViewModels.Home
 {
     public class HomeVM
     {
-        public ListCategoryForListVM Categories = new ListCategoryForListVM();
-        public List<ProductVM> Products = new List<ProductVM>();
+        public ListProductDetailsForUserVM Products = new();
     }
 }
