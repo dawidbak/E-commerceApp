c7db3ea8d6bfff4e299ff34130e8ae98ca3afaae
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-12 19:19:29 +0200

    Made view model for making List of ProductDetailsForUserVM

---
+++ src/EcommerceApp.Application/ViewModels/Product/ListProductDetailsForUserVM.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace EcommerceApp.Application.ViewModels.Product
+{
+    public class ListProductDetailsForUserVM
+    {
+        public List<ProductDetailsForUserVM> Products { get; set; }
+    }
+}
