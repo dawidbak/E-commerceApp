be9f1bcb5cf291c5b7c37703ff11a6992087d917
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-14 01:53:48 +0200

    Changed source mapping model and property name

--- src/EcommerceApp.Application/ViewModels/Cart/CartItemForListVM.cs
+++ src/EcommerceApp.Application/ViewModels/Cart/CartItemForListVM.cs
@@ -1,16 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 using System;
 using AutoMapper;
 using EcommerceApp.Application.Mapping;
 
 namespace EcommerceApp.Application.ViewModels.Cart
 {
-    public class CartItemForListVM : IMapFrom<Domain.Models.CartItem>
+    public class CartItemForListVM : IMapFrom<Domain.Models.Product>
     {
-        public int ProductId { get; set; }
+        public int Id { get; set; }
         public int Quantity { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public decimal UnitPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public decimal TotalPrice { get; set; }
 
-        public void Mapping(Profile profile) => profile.CreateMap<Domain.Models.CartItem, CartItemForListVM>();
+        public void Mapping(Profile profile) => profile.CreateMap<Domain.Models.Product, CartItemForListVM>();
     }
 }
