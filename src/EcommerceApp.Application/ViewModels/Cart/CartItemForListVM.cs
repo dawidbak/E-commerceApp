c3fe36116056a77e382a53f8a8036fe756dee0a9
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-15 15:56:03 +0200

    Clear mapping because it will be less effective in case get all cart items for user

--- src/EcommerceApp.Application/ViewModels/Cart/CartItemForListVM.cs
+++ src/EcommerceApp.Application/ViewModels/Cart/CartItemForListVM.cs
@@ -1,21 +1,24 @@
-using System.ComponentModel.DataAnnotations;
 using System;
+using System.ComponentModel.DataAnnotations;
 using AutoMapper;
 using EcommerceApp.Application.Mapping;
 
 namespace EcommerceApp.Application.ViewModels.Cart
 {
-    public class CartItemForListVM : IMapFrom<Domain.Models.Product>
+    public class CartItemForListVM
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C}")]
         public decimal UnitPrice { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C}")]
-        public decimal TotalPrice { get; set; }
-
-        public void Mapping(Profile profile) => profile.CreateMap<Domain.Models.Product, CartItemForListVM>();
+        public decimal TotalCartItemPrice
+        {
+            get => Quantity * UnitPrice;
+        }
     }
 }
