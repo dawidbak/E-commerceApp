0937258708279926cdaeedd8285e990dcfdde8df
Dawid Bak <75274385+dawidbak@users.noreply.github.com>
2021-06-15 15:53:44 +0200

    Added total price of cart value

--- src/EcommerceApp.Application/ViewModels/Cart/ListCartItemForListVM.cs
+++ src/EcommerceApp.Application/ViewModels/Cart/ListCartItemForListVM.cs
@@ -1,10 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcommerceApp.Application.ViewModels.Cart
 {
     public class ListCartItemForListVM
     {
         public List<CartItemForListVM> CartItems { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal TotalPrice
+        {
+            get
+            {
+                var result = 0m;
+                {
+                    foreach (var cartItem in CartItems)
+                    {
+                        result += cartItem.TotalCartItemPrice;
+                    }
+                }
+                return result;
+            }
+        }
     }
 }
